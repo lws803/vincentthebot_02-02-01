3b18f0750a26996552f6229f3f1ef254b20fce00
lekoook <kokteng1313@gmail.com>
2018-03-23 16:45:58 +0800

    Added serialize header

--- serializing_vincent/serialize.h
+++ serializing_vincent/serialize.h
@@ -1,2 +1,5 @@
-unsigned int serialize(void *p, size_t size)
-void sendSerialData(u_char *buffer, int len)
+#include <stdlib.h>
+
+unsigned int serialize(void *p, size_t size);
+void sendSerialData(u_char *buffer, int len);
+unsigned int deserialize(void *p, u_char *buf);
