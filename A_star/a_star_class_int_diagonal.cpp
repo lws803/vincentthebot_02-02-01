a889659e3e3d04aebea8a4194266831906b4bc95
lws803 <lws803@gmail.com>
2018-03-25 02:01:54 +0800

    Added some new extra features

--- A_star/a_star_class_int_diagonal.cpp
+++ A_star/a_star_class_int_diagonal.cpp
@@ -71,19 +71,27 @@ private:
     
     
     void scan_neighbours (int8_t maze[][MAX],
-                          coords_t* current, coords_t start, coords_t end) {
+                          coords_t* current, coords_t * start, coords_t * end) {
         int y = current->y, x = current->x;
         
         for (int i = -1; i < 2; i++) {
             for (int d = -1; d < 2; d++) {
                 if (x+i < MAX && x+i >= 0 && y+d < MAX && y+d >= 0 && !(i == 0 && d == 0)) {
-                    coords_t *neighbour = new coords_t;
-                    neighbour->x = x+i;
-                    neighbour->y = y+d;
-                    neighbour->g_cost = cartesian_count(x+i, y+d, start.x, start.y);
-                    neighbour->h_cost = cartesian_count(x+i, y+d, end.x, end.y);
-                    neighbour->parent = current;
-                    
+                    coords_t *neighbour;
+                    if (x + i == end->x && y + d == end->y) {
+                        // Just point the end to its parent
+                        neighbour = end;
+                        neighbour->g_cost = cartesian_count(x+i, y+d, start->x, start->y);
+                        neighbour->h_cost = cartesian_count(x+i, y+d, end->x, end->y);
+                        neighbour->parent = current;
+                    }else {
+                        neighbour = new coords_t;
+                        neighbour->x = x+i;
+                        neighbour->y = y+d;
+                        neighbour->g_cost = cartesian_count(x+i, y+d, start->x, start->y);
+                        neighbour->h_cost = cartesian_count(x+i, y+d, end->x, end->y);
+                        neighbour->parent = current;
+                    }
                     if (visited.find(make_pair(neighbour->x, neighbour->y)) == visited.end() && maze[y+d][x+i] <= probability ) {
                         open[neighbour->h_cost + neighbour->g_cost] = neighbour;
                         visited.insert(make_pair(neighbour->x, neighbour->y));
@@ -93,7 +101,7 @@ private:
         }
     }
     
-    int pathfinder (coords_t start, coords_t end, int8_t maze[][MAX]) {
+    int pathfinder (coords_t * start, coords_t * end, int8_t maze[][MAX]) {
         // Can only travel 4 directions
         while (!open.empty()) {
             coords_t *current = open.begin()->second;
@@ -102,7 +110,7 @@ private:
             
             closed.push_back(current);
             // pop(open, current);
-            if (current->x == end.x && current->y == end.y) {
+            if (current->x == end->x && current->y == end->y) {
                 return 1; // Path has been found
             }
             scan_neighbours (maze, current, start, end);
@@ -126,11 +134,31 @@ private:
         end->h_cost = 0;
         end->g_cost = cartesian_count(start->x, start->y, end->x, end->y);
     }
+    
+    int find_heading (coords_t * start, int8_t maze[][MAX]) {
+        for (int i = -1; i < 2; i++) {
+            for (int d = -1; d < 2; d++) {
+                if (!(i == 0 && d == 0) && maze[start->y + d][start->x + i] == -3) {
+                    // Found heading
+                    if (i == 0 && d == -1) return 0; // north
+                    else if (i == 0 && d == 1) return 180; // south
+                    else if (i == -1 && d == 0) return 270; // East
+                    else if (i == 1 && d == 0) return 90; // West
+                    else if (i == 1 && d == -1) return 45; // North West
+                    else if (i == 1 && d == 1) return 135; // South West
+                    else if (i == -1 && d == 1) return 225; // South East
+                    else if (i == -1 && d == -1) return 315; // North West
+                }
+            }
+        }
+        return -1;
+    }
+
 public:
     // Params to set
     int probability = 4;
-    int mode = 0; // 0 means manhattan distance, 1 means cartesian distance
-    
+    int mode = 1; // 0 means manhattan distance, 1 means cartesian distance
+    // Need to juggle between the two, sometimes its actually better to use this
     
     
     A_star (int h, int w) {
@@ -146,8 +174,8 @@ public:
     void generate_path () {
         int8_t maze[MAX][MAX] = {
             {5,5,5,5,5,5,5,5,5,5,5},
-            {5,0,0,0,0,5,0,5,0,0,5},
-            {5,5,0,0,0,0,0,0,0,0,5},
+            {5,0,0,0,0,0,0,0,0,0,5},
+            {5,0,0,0,0,0,0,0,0,0,5},
             {5,0,0,0,0,0,0,0,0,0,5},
             {5,0,0,0,0,0,0,0,0,0,5},
             {5,0,0,0,0,0,0,0,0,0,5},
@@ -160,6 +188,7 @@ public:
         
         coords_t *start = new coords_t;
         coords_t *end = new coords_t;
+        int steps = 1;
         // We can disable scan_maze
         
         start_end_scan(start, end, maze);
@@ -168,28 +197,26 @@ public:
         print_maze(maze);
         
         // Initial run
-        scan_neighbours (maze, start, *start, *end);
-        pathfinder(*start, *end, maze);
+        scan_neighbours (maze, start, start, end);
+        pathfinder(start, end, maze);
         
         // Traceback
         maze[end->y][end->x] = -3; // We use 9 to signify the path
-        coords_t* current_point;
-        // Search for end point (find end node)
-        for (auto iterator = closed.begin(); iterator != closed.end(); ++iterator) {
-            if ((*iterator)->x == end->x && (*iterator)->y == end->y) {
-                current_point = *iterator;
-                break;
-            }
-        }
+        coords_t* current_point = end;
+        
         // Iterate thru the next few
         while (current_point->x != start->x || current_point->y != start->y) {
             // Continue tracing
             current_point = current_point->parent;
             maze[current_point->y][current_point->x] = -3;
+            steps++;
+            // TODO: Count longest sustainable sequence here as well as this will ultimately lead us to count distance from start to junction
         }
         cout << "Shortest path:" << endl << endl;
         print_maze (maze);
-
+        cout << "Heading: " << find_heading(start, maze) << endl;
+        cout << "Total steps: " << steps << endl;
+        
     }
     
 };
